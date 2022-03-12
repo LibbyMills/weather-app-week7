a0fc7327f65f47cb753e0b788d619c2bb3c3692f
LibbyMills <millsjelizabeth@gmail.com>
2022-03-12 16:24:07 +0000

    Time formatted

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayData(response) {
   let cityElement = document.querySelector("#city");
   let tempNowElement = document.querySelector("#tempNow");
   let tempTodayMin = document.querySelector("#tempTodayMin");
   let wxDescription = document.querySelector("#wxDescription");
+  let dateElement = document.querySelector("#date");
 
   tempNowElement.innerHTML = Math.round(response.data.main.temp);
   tempTodayMin.innerHTML = Math.round(response.data.main.temp_min);
   wxDescription.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = response.data.name;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "bd3ff741f58b13df62ca6260d9e2d474";
