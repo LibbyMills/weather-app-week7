862b736e8e6df0a2e7fa2e784bdc51268f448326
LibbyMills <millsjelizabeth@gmail.com>
2022-03-21 11:47:03 +0000

    Removed F˚
    English based, removed unit conversion

--- src/app.js
+++ src/app.js
@@ -116,30 +116,5 @@ function processRequest(event) {
   search(cityInput.value);
 }
 
-function displayfahrenheit(event) {
-  event.preventDefault();
-  let fahrenheit = Math.round((celsiusTemp * 9) / 5 + 32);
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let tempNowElement = document.querySelector("#tempNow");
-  tempNowElement.innerHTML = fahrenheit;
-}
-
-function displayCelsius(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let tempNowElement = document.querySelector("#tempNow");
-  tempNowElement.innerHTML = celsiusTemp;
-}
-
-let celsiusTemp = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", processRequest);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayfahrenheit);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsius);
